d9d5b6783e3962dfb9b020a51b33ec111e181861
Matt Todd <matt.c.todd@gmail.com>
2014-10-11 06:14:46 -0500

    first linked list of structs

--- list.c
+++ list.c
@@ -1,7 +1,40 @@
 #include "stdio.h"
+#include "stdlib.h"
+
+struct Node
+{
+   int value;
+   struct Node *next;
+};
+
+struct List
+{
+   struct Node *first;
+};
 
 int main() 
 {
-   printf("hello world!\n");
+   struct List *mylist = malloc(sizeof(struct List));
+   struct Node *node1 = malloc(sizeof(struct Node));
+   struct Node *node2 = malloc(sizeof(struct Node));
+   struct Node *node3 = malloc(sizeof(struct Node));
+
+   mylist->first = node1;
+   node1->value = 1;
+   node1->next = node2;
+   node2->value = 2;
+   node2->next = node3;
+   node3->value = 3;
+   node3->next = NULL;
+
+   printf("values in the list:\n");
+   struct Node *current = mylist->first;
+   while (current != NULL)
+   {
+      printf("  %d \n", current->value);
+      current = current->next;
+   }
+
+   free(mylist);
    return 0;
 }
